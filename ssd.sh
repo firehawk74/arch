430f929d59d6ba220edb32ba2f9d80a41bdd7aeb
firehawk74 <85374048+firehawk74@users.noreply.github.com>
2021-09-04 22:40:23 +0200

    Update ssd.sh

--- ssd.sh
+++ ssd.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-DYSK=/dev/sda
+echo Example: sda
 
-#  rozmiary partycji w MiB
-PART_EFI=512       #512 MiB
-PART_ROOT=43.9    #40 GiB
-PART_SWAP=11.9     #~8 GiB
+read -p '/dev/' hardDrivevar
+
+fdisk /dev/$hardDrivevar << EOF
+g
+n
+1
+
++550M
+n
+2
 
 
-echo "label: gpt
-device: ${sda}
-unit: sectors
+t
+1
+83
+t
+2
+82
 
-${sda}1 : size=${PART_EFI}MiB, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
-${sda}2 : size=${PART_ROOT}GiB, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
-${sda}3 : size=${PART_SWAP}GiB, type=0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
-" | sfdisk ${sda}
-
+w
+EOF
 
 
 mkfs.vfat -F32 -n EFI /dev/sda1
@@ -26,6 +32,5 @@ mkswap -L swap /dev/sda3
 
 mount /dev/sda2 /mnt
 swapon /dev/sda3
-
 
 
