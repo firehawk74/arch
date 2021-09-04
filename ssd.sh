4da786f76c8dbc8676c479b54a7d7ccf1fbbaab9
firehawk74 <85374048+firehawk74@users.noreply.github.com>
2021-09-04 22:07:07 +0200

    Update ssd.sh

--- ssd.sh
+++ ssd.sh
@@ -12,9 +12,9 @@ echo "label: gpt
 device: ${sda}
 unit: sectors
 
-${sda}1 : size=${512}MiB, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
-${sda}2 : size=${43.9}GiB, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
-${sda}3 : size=${11.9}GiB, type=0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
+${sda1}1 : size=${512}MiB, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+${sda2}2 : size=${43.9}GiB, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
+${sda3}3 : size=${11.9}GiB, type=0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
 " | sfdisk ${sda}
 
 
