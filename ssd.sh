1f13f2a6c0d8bf181bd112ccff21832f03c3ea8f
firehawk74 <85374048+firehawk74@users.noreply.github.com>
2021-09-04 23:45:51 +0200

    Update ssd.sh

--- ssd.sh
+++ ssd.sh
@@ -5,24 +5,31 @@ echo Example: sda
 read -p '/dev/' hardDrivevar
 
 fdisk /dev/$hardDrivevar << EOF
-g
-n
-1
+g               # create new GPT partition
+n               # add new partition
+1               # partition number
+                # default - first sector 
++550M           # partition size
+t               # change partition type
+1               # partition number
+n               # add new partition
+2               # partition number
 
-+550M
-n
-1
+
++44G           # partition size
+t               # change partition type
+1               # partition number
+20              # Linux filesystem
+n               # add new partition
+3               # partition number
 
 
-t
-1
-1
-t
-2
-20
-t
-3
-19
+#t               # change partition type
+#2               # partition number
+#20              # Linux filesystem
+#t               # change partition type
+#3               # partition number
+#19              # Linux filesystem
 
 w
 EOF
@@ -30,10 +37,12 @@ EOF
 
 mkfs.vfat -F32 -n EFI /dev/sda1
 mkfs.ext4 -L root /dev/sda2
-mkswap -L swap /dev/sda3
+#mkswap -L swap /dev/sda3
+#mkfs.ext4 -L dane /dev/sda3
 
 
 mount /dev/sda2 /mnt
-swapon /dev/sda3
-
+#swapon /dev/sda3
+#mkdir /mnt/home 
+#mount /dev/sda3 /mnt/dane
 
