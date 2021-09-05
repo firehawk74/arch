c0f46b95b68caff26fe624c3be112a007be2f7eb
firehawk74 <85374048+firehawk74@users.noreply.github.com>
2021-09-05 08:23:13 +0200

    Update ssd.sh

--- ssd.sh
+++ ssd.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-DYSK=/dev/sdx
+DYSK=/dev/sda
 
 #rozmiary partycji w MiB
 PART_EFI=512       #512 MiB
 PART_ROOT=43500    #43.5 GiB
-PART_HOME=223570   #240 GiB
 PART_SWAP=11900    #~12 GiB
-
 
 echo "label: gpt
 device: ${sda}
@@ -17,6 +15,11 @@ ${sda}1 : size=${512}MiB, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
 ${sda}2 : size=${43500}MiB, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
 ${sda}3 : size=${11900}MiB, type=0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
 " | sfdisk ${sda}
+
+DYSK=/dev/sdb
+
+#rozmiary partycji w MiB
+PART_HOME=223570   #240 GiB
 
 echo "label: gpt
 device: ${sdb}
