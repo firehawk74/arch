65bd512d333dddf9de83df9531aeb8277a7fb2d2
firehawk74 <85374048+firehawk74@users.noreply.github.com>
2021-09-05 08:16:21 +0200

    Update ssd.sh

--- ssd.sh
+++ ssd.sh
@@ -1,48 +1,44 @@
 #!/bin/bash
 
-echo Example: sda
+DYSK=/dev/sdx
 
-read -p '/dev/' hardDrivevar
-
-fdisk /dev/$hardDrivevar << EOF
-g               # create new GPT partition
-n               # add new partition
-1               # partition number
-                # default - first sector 
-+550M           # partition size
-t               # change partition type
-1               # partition number
-n               # add new partition
-2               # partition number
+#rozmiary partycji w MiB
+PART_EFI=512       #512 MiB
+PART_ROOT=43500    #43.5 GiB
+PART_HOME=223570   #240 GiB
+PART_SWAP=11900    #~12 GiB
 
 
-+44G           # partition size
-t               # change partition type
-1               # partition number
-20              # Linux filesystem
-n               # add new partition
-3               # partition number
+echo "label: gpt
+device: ${sda}
+unit: sectors
+
+${sda}1 : size=${512}MiB, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+${sda}2 : size=${43500}MiB, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
+${sda}3 : size=${11900}MiB, type=0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
+" | sfdisk ${sda}
+
+echo "label: gpt
+device: ${sdb}
+unit: sectors
+
+${sdb}1 : size=${223570}MiB, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
+" | sfdisk ${sdb}
 
 
-#t               # change partition type
-#2               # partition number
-#20              # Linux filesystem
-#t               # change partition type
-#3               # partition number
-#19              # Linux filesystem
+#typy partycji możesz sobie sprawdzić przy pomocy sfdisk -X gpt -T
 
-w
-EOF
+#formatowanie standardowo przez mkfs
 
 
 mkfs.vfat -F32 -n EFI /dev/sda1
 mkfs.ext4 -L root /dev/sda2
-#mkswap -L swap /dev/sda3
-#mkfs.ext4 -L dane /dev/sda3
+mkfs.ext4 -L dane /dev/sdb1
+mkswap -L swap /dev/sda3
 
 
 mount /dev/sda2 /mnt
-#swapon /dev/sda3
-#mkdir /mnt/home 
-#mount /dev/sda3 /mnt/dane
+mkdir /mnt/home 
+mount /dev/sdb1 /mnt/dane
+swapon /dev/sda3
 
