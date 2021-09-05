6a65c10f2fea8ff11f61ebc31442493fc5a8608d
firehawk74 <85374048+firehawk74@users.noreply.github.com>
2021-09-05 10:35:07 +0200

    Update arch-zen.sh

--- arch-zen.sh
+++ arch-zen.sh
@@ -3,7 +3,7 @@ loadkeys pl
 setfont Lat2-Terminus16
 timedatectl set-ntp true
 
-pacstrap -i /mnt base base-devel bash-completion intel-ucode iucode-tool linux-zen linux-zen-headers linux-firmware nano dhcpcd neofetch
+pacstrap -i /mnt base base-devel bash-completion linux-zen linux-zen-headers linux-firmware nano dhcpcd neofetch
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
