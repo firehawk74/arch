ba749f35c6d052fd82b93fec7ec6bc6cfe346f16
firehawk74 <85374048+firehawk74@users.noreply.github.com>
2021-09-05 10:26:42 +0200

    Create arch-base.sh

---
+++ arch-base.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+loadkeys pl
+setfont Lat2-Terminus16
+timedatectl set-ntp true
+
+pacstrap -i /mnt base base-devel bash-completion intel-ucode iucode-tool linux-zen linux-zen-headers linux-firmware nano dhcpcd neofetch
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt/
+
